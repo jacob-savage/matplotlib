c8a5efa146422e746a9f0bc59479331667b6d3d5
Jacob Savage <savagejacob13@gmail.com>
2023-03-18 13:02:16 +1100

    jup

--- testing.py
+++ testing.py
@@ -1,6 +1,43 @@
-import matplotlib as plt
+#%% 
+# from matplotlib.pylab import *
+# x = arange(0, 10)
+# scatter(x, x, c=x, facecolors='none')
 
 
-from matplotlib.pylab import *
-x = arange(0, 10)
-scatter(x, x, c=x, facecolors='none')
+
+
+
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+x = np.arange(0, 10)
+norm = plt.Normalize(0, 10)
+cmap = mpl.colormaps['viridis']
+cols = cmap(norm(x))
+plt.scatter(x, x, facecolors='none', edgecolors=cols)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# %%
