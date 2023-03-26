9164dfab5cd7dd0e830ff4829fde90408925552d
Jacob Savage <savagejacob13@gmail.com>
2023-03-26 12:05:24 +1100

    Update testing.py

--- testing.py
+++ testing.py
@@ -7,36 +7,13 @@
 
 
 
-
-
-import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-
-x = np.arange(0, 10)
-norm = plt.Normalize(0, 10)
-cmap = mpl.colormaps['viridis']
-cols = cmap(norm(x))
-plt.scatter(x, x, facecolors='none', edgecolors=cols)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+xs, ys = [1,2,3], [2,3,1]
+fig, ax = plt.subplots(3)
+ax[0].scatter(xs, ys, label='loc-tuple-arg')
+ax[0].legend(loc=(1.1, .5, 1.1, "abc"))
+plt.show()
 
 
 
